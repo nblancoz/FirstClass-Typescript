bf40106a66e541ffc4842bcc6b6f039d283d4f48
nblancoz <nblancozappala@gmail.com>
2023-12-12 10:45:24 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+"use strict";
+let nombre = "Nicolas";
+// console.log(nombre);
+let id = 5;
+let prueba = "prueba";
+let isPublished = true;
+let x = "hola"; // sirve para cualquier tipo
+let z;
+z =
+    // 22
+    "22";
+// console.log(id, prueba, isPublished, x, z);
+let ids = [1, 2, 3, 4, 5];
+// let ids: number[];
+// ids = [1, 2, 3, 4, 5, 6]
+let arr = [20, true, "Ni"];
+let arr2 = [6, "Nicolas"];
+// console.log(ids, arr, arr2)
+// tuplas --> con un orden
+let person = [19, "Nicolas", true]; // el array de los tipos de datos recibe los valores con ese mismo order, no se puede poner primer el string y despues el number
+// let person: [number, string, boolean] = ["Nicolas", 19, true] --> error!
+let students;
+students = [
+    [1, "Nico"],
+    [2, "Álvaro"],
+    [3, "Demis"],
+];
+const user = {
+    id: 1,
+    name: "Nicolas",
+};
+const user2 = {
+    id: 2,
+    name: "Nicolas2",
+};
+const user3 = {
+    id: 3,
+    name: "Nicolas3",
+    //   age: 19 --> es opcional
+};
+const grace = {
+    name: "Grace",
+    legs: 4,
+    isDogFriendly: true,
+    breed: "Chucho", // se incluye despues de hacer el extend
+};
+function addNum(x, y) {
+    // se tipan los datos que llegan y lo que devuelve, por eso tres "number"
+    return x + y;
+}
+// function addNum(x: number, y: number): number {
+//   return "x + y"; --> error
+// }
+// console.log(addNum(2, 3));
+// console.log(addNum(2, "3")) --> error
+function showName(name) {
+    // :string porque devuelve un string
+    return name;
+}
+function test(name) {
+    // se pone el void si no hay ningun return
+    console.log(name);
+}
+// const dog1 = {
+//   age: 2,
+//   name: "Zenda",
+// };
+// const dog2 = {
+//   age: 2,
+//   name: "Tony",
+// };
+// se repite la misma estructura en dog1 y dog2, por ello se crea una clase
+class Dog {
+    constructor(age, name) {
+        this.age = age;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const toni = new Dog(11, "Toni");
+const zenda = new Dog(11, "Zenda");
